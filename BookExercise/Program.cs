d8992016405a0868b59d9666e5237645d4155a9f
Alex <Alex@LAPTOP-2Q2DRBGK>
2026-01-08 09:24:16 +0200

    Add project files.

---
+++ BookExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BookExercise
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private string id;
+        private double price;
+
+        public Book()
+        {
+            title = "";
+            author = "";
+            SetId(id);
+            price = 0.0;
+        }
+    }
+}
---
+++ CarExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CarExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
