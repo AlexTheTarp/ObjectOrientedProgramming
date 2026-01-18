74be27d11c6b6f0ccb5d0ced3cd8b78441b00b46
Alex <Alex@ANNA>
2026-01-18 15:32:21 +0200

    Kissa

--- EmployeeExercise/Program.cs
+++ EmployeeExercise/Program.cs
@@ -1,10 +1,13 @@
 ﻿using EmployeeExercise;
 using System;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = Encoding.UTF8;
+
         Console.WriteLine();
 
         Employee[] employees = new Employee[3];
